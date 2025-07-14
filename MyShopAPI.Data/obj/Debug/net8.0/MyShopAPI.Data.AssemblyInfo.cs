85c131bc08eee0aeabed1469efe26b38923c66c3
Olumide Rotimi <rotimiolumide68@gmail.com>
2025-07-14 21:59:42 +0100

    Altered EFPostgres version

--- MyShopAPI.Data/obj/Debug/net8.0/MyShopAPI.Data.AssemblyInfo.cs
+++ MyShopAPI.Data/obj/Debug/net8.0/MyShopAPI.Data.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MyShopAPI.Data")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5793d57b950ffc63d600f62643fc448460196f34")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e7fb77e6cf83a55623b38eeffa7d1012afb7b13e")]
 [assembly: System.Reflection.AssemblyProductAttribute("MyShopAPI.Data")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MyShopAPI.Data")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- MyShopAPI.Services/obj/Debug/net8.0/MyShopAPI.Services.AssemblyInfo.cs
+++ MyShopAPI.Services/obj/Debug/net8.0/MyShopAPI.Services.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MyShopAPI.Services")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5793d57b950ffc63d600f62643fc448460196f34")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e7fb77e6cf83a55623b38eeffa7d1012afb7b13e")]
 [assembly: System.Reflection.AssemblyProductAttribute("MyShopAPI.Services")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MyShopAPI.Services")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
