5aa973d1254079019871ee8378e6ffd7e69ccd31
Olumide Rotimi <rotimiolumide68@gmail.com>
2025-07-15 17:40:42 +0100

    Enforced unification of Microsoft.IdentityModel.Protocols.OpenIdConnect version

--- MyShopAPI.Core/obj/Debug/net8.0/MyShopAPI.Core.AssemblyInfo.cs
+++ MyShopAPI.Core/obj/Debug/net8.0/MyShopAPI.Core.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MyShopAPI.Core")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+61089db37e9a508459713e0906cc0676a81bc694")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ac2b8dac2e452b96d74750745293f78874df4b7a")]
 [assembly: System.Reflection.AssemblyProductAttribute("MyShopAPI.Core")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MyShopAPI.Core")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- MyShopAPI.Services/obj/Debug/net8.0/MyShopAPI.Services.AssemblyInfo.cs
+++ MyShopAPI.Services/obj/Debug/net8.0/MyShopAPI.Services.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MyShopAPI.Services")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+61089db37e9a508459713e0906cc0676a81bc694")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ac2b8dac2e452b96d74750745293f78874df4b7a")]
 [assembly: System.Reflection.AssemblyProductAttribute("MyShopAPI.Services")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MyShopAPI.Services")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
