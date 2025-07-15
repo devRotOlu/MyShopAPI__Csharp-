61089db37e9a508459713e0906cc0676a81bc694
Olumide Rotimi <rotimiolumide68@gmail.com>
2025-07-15 16:23:49 +0100

    Forced version unification of System.IdentityModel.Tokens.Jwt in all projects

--- MyShopAPI.Services/obj/Debug/net8.0/MyShopAPI.Services.AssemblyInfo.cs
+++ MyShopAPI.Services/obj/Debug/net8.0/MyShopAPI.Services.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MyShopAPI.Services")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+51483b0639bde10c7f143720b40ea5b98651ffa4")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e401ae6d575ca88d034833d8d9d83b84c11a40b4")]
 [assembly: System.Reflection.AssemblyProductAttribute("MyShopAPI.Services")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MyShopAPI.Services")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
